7618515b65d7fdbe008b63d4fcf81fee5924daae
treasureBear94 <qvo7896@gmail.com>
2019-04-11 15:13:40 +0900

    프로젝트 생성

---
+++ src/main/java/com/treabear/webservice/web/WebRestController.java
@@ -0,0 +1,17 @@
+package com.treabear.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * WebRestController
+ */
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+    
+}
