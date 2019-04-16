4d8a5cd418d5e5b94fdf409e43ae235e2f443620
treasureBear94 <qvo7896@gmail.com>
2019-04-16 19:42:04 +0900

    수정

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,4 +1,4 @@
 # CodeDeploy에서 바로 deploy.sh를 실행시킬 수 없어, 우회하는 방법으로 deploy.sh를 실행하는 execute-deploy.sh 파일
 
 #!/bin/bash
-/home/ec2-user/app/travis/deploy.sh > /dev/null 2> /dev/null < /dev/null &
+/home/ec2-user/app/nonstop/deploy.sh > /dev/null 2> /dev/null < /dev/null &
