ae1198b88169f0420fe1eef57297acf5a85ff4e7
treasureBear94 <qvo7896@gmail.com>
2019-04-17 11:33:19 +0900

    application.yml 위치 수정

--- src/main/java/com/treabear/webservice/Application.java
+++ src/main/java/com/treabear/webservice/Application.java
@@ -11,8 +11,8 @@ public class Application {
 	// 실행 환경을 프로젝트 외부에 설정
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 								+ "classpath:application.yml,"
-								// + "/app/config/spring-webservice/real-application.yml";
-								+ "C:\\dev\\yj\\app\\config\\spring-webservice\\real-application.yml";
+								+ "/app/config/spring-webservice/real-application.yml";
+								// + "C:\\dev\\yj\\app\\config\\spring-webservice\\real-application.yml";
 
 	public static void main(String[] args) {
 		// SpringApplication.run(Application.class, args);
